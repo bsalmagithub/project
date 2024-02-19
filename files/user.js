1a812f9360b8f0325a5ea4e4a5c1f6485769ba28
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2024-02-19 09:22:42 +0000

    user

---
+++ files/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    // Other user details
+});
+
+module.exports = mongoose.model('User', userSchema);
